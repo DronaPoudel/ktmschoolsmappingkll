8746601d028055c23e638f06f27c7cae8ccab71c
Drona Poudel <pdronachandra@gmail.com>
2020-02-01 14:27:49 +0545

    geojson data not rendering fixed

--- src/App.js
+++ src/App.js
@@ -1,112 +1,61 @@
 import React, { Component } from 'react';
 import './App.css';
-// import L from 'leaflet';
-// import GeoJSON from 'leaflet';
-// import './ktmschools';
-// import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import { Map, TileLayer} from 'react-leaflet';
+import L from 'leaflet';
+// import { Map, TileLayer } from 'react-leaflet';
+import Geojsondata from './ktmschools.json';
 
+// var map = L.map('map', {
+//   zoom: 13
+// });
 
-// var myGeojsonlayer = L.geoJSON().addTo(map);
-// myGeojsonlayer.addData(geojsonFeature);
-
-type State = {
-  lat: number,
-  lng: number,
-  zoom: number,
-}
-
-// marker Icon
 // const myIcon = L.icon({
 //   iconUrl: 'images/marker-icon.png',
 //   iconSize: [50, 32],
 //   iconAnchor: [25, 16]
-// })
+// });
 
-// class ktmGeojson extends Component {
-//   render() { 
-//     return ( L.geoJSON(ktmschools, {
-//       style : function(feature) {
-//         return {
-//           color: '#e0e0',
-//         }
-//       }
-//     }).addTo(map),
-//     )
-//   }
-// }
- 
-// export default ktmGeojson;
+  // state = {
+  //   lat: 27.7172,
+  //   lng: 85.3240,
+  //   zoom: 13,
+  // }
 
-// class App extends Component {
-  export default class SimpleExample extends Component<{}, State> {
+class mapapp extends  Component {
+  componentDidMount() {
+    let map = L.map('map').setView([27.7172, 85.3240], 13);
+    console.clear()
+console.log(Geojsondata)
 
-  state = {
-    lat: 27.7172,
-    lng: 85.3240,
-    zoom: 13,
+   function onclicking (feature, layer) {
+     layer.bindPopup(feature.properties.name);
+   };
+
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map)
+
+    L.geoJson(Geojsondata, 
+      {
+      onEachFeature: onclicking
+
+    }).addTo(map);
+    
+  
   }
-
-  render(){
-
-    // const map = L.map('map').setView([51.505, -0.09], 13);
-    const position = [this.state.lat, this.state.lng]
+  
+  render() { 
+    
     return (
-      React.createElement('div', {id: 'leafletmap'}, 'code'),
-
-      // var map = L.map('mapid', {
-      //   zoomcontrol: false
-      // }).addTo(map);
-      <div className="App">
-        <header className="">
-          <h1>Schools in Kathmandu</h1>
-        </header>
-  
-        <div id="mapid">
-        {/* L.geoJSON(ktmschools, {
-        style : function(feature) {
-          return {
-            color: '#e0e0';
-          }
-        }
-      }).addTo(map); */}
-
-          <Map center={position} zoom={this.state.zoom}>
-          <TileLayer
-            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />.addTo(map);
-
-      
-          {/* L.marker([0, 0], {icon: myIcon}).addTo(map); */}
-          
-          {/* <Marker position = {position} icon = {myIcon}>
-            <Popup>
-              A preety CSS£ popup.
-            </Popup>
-          </Marker> */}
-          </Map>
-      
-
-          {/* L.marker([51.5, -0.09]).addTo(map)
-              .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-              .openPopup(); */}
-
-
-        </div>
-  
+      <React.Fragment>
+      <h1 className = "page-header">Schools In Kathmandu</h1>
+      <div id='map'>
       </div>
-      // function osmschools (feature, layer){
-      // };
-      // <GeoJSON data = {ktmschools} />
+    </React.Fragment>
 
-      // L.geoJSON(ktmschools, {
-      //   onEachFeature: ;
-      // }.addTo(map);
+    )
 
-      
-    );
   }
-}
-
-// export default App;
+  } 
+export default mapapp;
+ 
