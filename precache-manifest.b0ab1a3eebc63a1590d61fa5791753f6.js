09dea1a4c19b2fbb424c1a1f2e0882d523bcb78b
Dron <cuppon.drona@gmail.com>
2021-01-10 12:32:13 +0545

    Updates

---
+++ precache-manifest.b0ab1a3eebc63a1590d61fa5791753f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f6f46b082331fab8cbbe6009318c96",
+    "url": "/DronaPoudel/ktmschoolsmappingkll/index.html"
+  },
+  {
+    "revision": "fc5f00ad28d065b0e057",
+    "url": "/DronaPoudel/ktmschoolsmappingkll/static/css/2.36f280e5.chunk.css"
+  },
+  {
+    "revision": "fc8c94aaa29e9c005608",
+    "url": "/DronaPoudel/ktmschoolsmappingkll/static/css/main.3bb4d2ed.chunk.css"
+  },
+  {
+    "revision": "fc5f00ad28d065b0e057",
+    "url": "/DronaPoudel/ktmschoolsmappingkll/static/js/2.9e0f03f1.chunk.js"
+  },
+  {
+    "revision": "5d902a352ffd255227de11674b8784c8",
+    "url": "/DronaPoudel/ktmschoolsmappingkll/static/js/2.9e0f03f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc8c94aaa29e9c005608",
+    "url": "/DronaPoudel/ktmschoolsmappingkll/static/js/main.b4d792de.chunk.js"
+  },
+  {
+    "revision": "dd8ae3e70768627b5e68",
+    "url": "/DronaPoudel/ktmschoolsmappingkll/static/js/runtime-main.109a0e61.js"
+  }
+]);
